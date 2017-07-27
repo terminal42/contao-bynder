eb8245c13c5ede5d69f6d57257dbedf528cbb88c
Yanick Witschi <yanick.witschi@terminal42.ch>
2017-07-27 19:43:18 +0200

    Added bynder_id to tl_files

---
+++ src/Resources/contao/dca/tl_files.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_files']['config']['sql']['keys']['bynder_id'] = 'unique';
+$GLOBALS['TL_DCA']['tl_files']['fields']['bynder_id']['sql'] = ['type' => 'string', 'length' => 64, 'notnull' => false];
