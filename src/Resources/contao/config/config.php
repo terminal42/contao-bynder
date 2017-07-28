002b302a443aba358ae7101b2432e1e5c097357d
Yanick Witschi <yanick.witschi@terminal42.ch>
2017-07-28 16:13:44 +0200

    Removed event listener because we're manually downloading the images now

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -11,8 +11,3 @@ if (TL_MODE === 'BE') {
     $GLOBALS['TL_CSS'][] = 'bundles/terminal42contaobynder/styles.min.css|static';
     $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/terminal42contaobynder/app.min.js|static';
 }
-
-/*
- * Hooks
- */
-$GLOBALS['TL_HOOKS']['executePreActions'][] = ['terminal42.contao_bynder.event_listener.import_images', 'onExecutePreActions'];
