bbab640905955bda3696c5c54a60838383d6406d
Yanick Witschi <yanick.witschi@terminal42.ch>
2019-01-09 16:14:00 +0100

    Fixed more compat issues

--- src/Api.php
+++ src/Api.php
@@ -15,8 +15,7 @@ use Bynder\Api\Impl\Oauth\OauthRequestHandler;
 
 /**
  * This is just here because the php-sdk of bynder does not allow you to access
- * the request handler directly which basically prevents you from doing anything
- * with the api that is not supported by the sdk yet...
+ * the base url
  */
 class Api extends BynderApi
 {
@@ -26,56 +25,5 @@ class Api extends BynderApi
     public function getBaseUrl()
     {
         return $this->baseUrl;
-    }
-
-    /**
-     * @return OauthRequestHandler
-     */
-    public function getRequestHandler()
-    {
-        return $this->requestHandler;
-    }
-
-    /**
-     * Creates an instance of BynderApi using the settings provided.
-     *
-     * @param array $settings oauth credentials and settings to configure the BynderApi instance
-     *
-     * @throws \InvalidArgumentException oauth settings not valid, consumer key or secret not in array
-     *
-     * @return BynderApi instance
-     */
-    public static function create($settings)
-    {
-        if (isset($settings) && ($settings = self::validateSettings($settings))) {
-            $credentials = new Credentials(
-                $settings['consumerKey'],
-                $settings['consumerSecret'],
-                $settings['token'],
-                $settings['tokenSecret']
-            );
-            $requestHandler = OauthRequestHandler::create($credentials, $settings['baseUrl']);
-
-            return new static($settings['baseUrl'], $requestHandler);
-        }
-        throw new \InvalidArgumentException('Settings passed for BynderApi service creation are not valid.');
-    }
-
-    /**
-     * Checks if the settings array passed is valid.
-     *
-     * @param $settings
-     *
-     * @return bool whether the settings array is valid
-     */
-    private static function validateSettings($settings)
-    {
-        if (!isset($settings['consumerKey']) || !isset($settings['consumerSecret'])) {
-            return false;
-        }
-        $settings['token'] = isset($settings['token']) ? $settings['token'] : null;
-        $settings['tokenSecret'] = isset($settings['tokenSecret']) ? $settings['tokenSecret'] : null;
-
-        return $settings;
     }
 }
